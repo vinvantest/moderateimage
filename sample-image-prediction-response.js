5e3c5307ef4f345a1a42963e2aaaaf0626e15cbb
Vinayak Vanarse <30611090+vinvantest@users.noreply.github.com>
2021-11-11 07:18:30 +1100

    created sample response for reference

---
+++ sample-image-prediction-response.js
@@ -0,0 +1,35 @@
+{
+    "result": {
+        "predictions": [
+            {
+                "acceptable": "99.4020"
+            },
+            {
+                "acceptable-sketch": "0.4396"
+            },
+            {
+                "porn": "0.0774"
+            },
+            {
+                "sexy": "0.0427"
+            },
+            {
+                "porn-hentai": "0.0383"
+            }
+        ],
+        "classifications": [
+            {
+                "type": "pier",
+                "probability": "35.5212"
+            },
+            {
+                "type": "steel arch bridge",
+                "probability": "8.5169"
+            },
+            {
+                "type": "umbrella",
+                "probability": "8.2623"
+            }
+        ]
+    }
+}
